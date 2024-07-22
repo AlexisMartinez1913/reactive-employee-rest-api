229d74836a369dacfd1731239d67b9d1d2bdd75b
Alexis Martinez <jhonyalexis100@gmail.com>
2024-07-22 15:10:47 -0500

    init project

---
+++ src/test/java/jagarcia/springboot/webflux/SpringbootWebfluxTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package jagarcia.springboot.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
